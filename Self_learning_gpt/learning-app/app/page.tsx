4113207658aa93c7e68d9641f8e5efc45cef31ef
Nitish pandey <Nitish7040@users.noreply.github.com>
2025-02-10 20:11:14 +0530

    make some changes in learning app

--- Self_learning_gpt/learning-app/app/page.tsx
+++ Self_learning_gpt/learning-app/app/page.tsx
@@ -2,7 +2,8 @@
 export default function Home() {
   return (
     <>
-      <h1 className="text-3xl font-thin">code with nitish</h1>
+      <h1 className="text-3xl font-thin
+      ">code with nitish</h1>
     </>
   );
 }
