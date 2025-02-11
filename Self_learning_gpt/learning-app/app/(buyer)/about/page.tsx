d26ceb4dbbf318ea6e48decb616c1dd8ff41dcba
Nitish pandey <Nitish7040@users.noreply.github.com>
2025-02-11 21:59:50 +0530

    adding route group and loading

--- Self_learning_gpt/learning-app/app/(buyer)/about/page.tsx
+++ Self_learning_gpt/learning-app/app/(buyer)/about/page.tsx
@@ -3,7 +3,7 @@ import { error } from "console";
 // app/about/page.tsx
 export default function AboutPage() {
 
-  throw error("This is an error");
+
     return <h1>About Page Content of buyer</h1>;
   }
   
--- Self_learning_gpt/learning-app/app/(seller)/sellerabout/page.tsx
+++ Self_learning_gpt/learning-app/app/(seller)/sellerabout/page.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 
 export default function page() {
-  throw error("This is an error from the seller about page");
+
   return (
     <div>page from seller about</div>
   )
